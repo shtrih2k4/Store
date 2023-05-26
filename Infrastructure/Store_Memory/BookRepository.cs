6180bcede8965206e04685279699149eebfd010f
shtrih2k4 <andriizhevahin@gmail.com>
2023-05-26 19:07:41 +0300

    Added first classes and controllers

---
+++ Infrastructure/Store_Memory/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿using Store;
+
+namespace Store_Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly List<Book> books=new List<Book>() 
+        {
+            new Book(1,"Art of Programming"),
+            new Book(2,"Refactoring"),
+            new Book(3,"C Programming Language")
+        };
+        public List<Book> GetAllByTitle(string title_part)
+        {
+            return books.Where(book => book.Title.Contains(title_part)).ToList();
+        }
+    }
+}
---
+++ domain/Store/Book.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Store
+{
+    public class Book
+    {
+        public uint Id { get; }
+        public string Title { get;} = null!;
+        public Book(uint id,string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+    }
+}
